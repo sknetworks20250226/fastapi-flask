e35aa286508e6317c54da63e833b24db23435150
sknetworks20250226 <sknetworks.20250226@gmail.com>
2025-06-16 12:37:46 +0900

    .

--- api/main.py
+++ api/main.py
@@ -145,6 +145,7 @@ def place_order(order: OrderRequest, db:Session=Depends(get_db)):
         db.delete(item)  # cart 테이블에서 삭제        
     db.commit()
     return {"success":True, 'message':'주문이 완료 되었습니다'}
+
 #주문 목록 조회
 @app.get('/api/order', response_model=List[OrderOut])
 def get_orders(user_id:int = Query(...),db:Session=Depends(get_db)):
